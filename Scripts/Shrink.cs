08995378dd687b02ce50de95732a9ca5fd1b1791
horoneko <mail@horoneko.net>
2020-04-12 13:54:48 +0900

    最初のコミット

---
+++ Scripts/Shrink.cs
@@ -0,0 +1,35 @@
+
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Components;
+using VRC.SDKBase;
+using VRC.Udon;
+
+
+
+public class Shrink : UdonSharpBehaviour
+{
+
+    GameObject root;
+    UdonBehaviour script;
+
+    void Start()
+    {
+        root = transform.root.gameObject;
+        script = (UdonBehaviour)root.GetComponent(typeof(UdonBehaviour));
+    }
+    
+
+    public override void Interact()
+    {
+        script.SendCustomEvent("CountDown");
+    }
+    
+    private void OnMouseDown() {
+        VRCPlayerApi localPlayer = Networking.LocalPlayer;
+
+        // プレイヤーがいない ＝ エディターで起動した場合はクリック操作もできるようにする
+        if (localPlayer == null) script.SendCustomEvent("CountDown");
+    }
+
+}
